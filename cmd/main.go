6045fd5edebf039ffb5d083d99ee72e15d562336
rohit20001221 <rohit20001221@gmail.com>
2024-10-02 22:09:07 +0530

    updated the example code

--- cmd/main.go
+++ cmd/main.go
@@ -19,9 +19,9 @@ func main() {
 		const response = http.get("https://httpbin.zcorky.com/get")
 
 		log("response", http.json(response));
-		log("username", env.getEnv("USERNAME"))
+		log("pwd", env.getEnv("PWD"))
 
 		const buffer = fs.read("go.mod")
-		log("data", TextDecoder)
+		log("data", buffer)
 	`)
 }
